2ce83aaac9aca45fa39714f0512fa9c7e45db80b
x1111101101 <x1111101101@gmail.com>
2024-03-30 14:48:22 +0900

    Signed-off-by: x1111101101 <x1111101101@gmail.com>

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const session = require('express-session');
+const passport = require("passport")
+const GoogleStrategy = require('passport-google-oauth20').Strategy 
+const fs = require('fs');
+const crypto = require('crypto');
+
+const app = express()
+app.use(session({
+    secret: crypto.randomBytes(32).toString('hex'),
+    resave: false,
+    saveUninitialized: true
+}));
+function initPassport() {
+    app.use(passport.initialize());
+    app.use(passport.session());
+    let json = fs.readFileSync("keys/oauth", 'utf8');
+    let oauth = JSON.parse(json)
+    // Google OAuth 설정
+    passport.use(new GoogleStrategy({
+        clientID: oauth.id,
+        clientSecret: oauth.key,
+        callbackURL: 'http://playchess.kro.kr:8080/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        // 사용자 정보가 profile에 들어 있습니다.
+        return done(null, profile);
+    }));
+}
+initPassport()
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.listen(8080, ()=>{
+    console.log("listening on 8080")
+})
+
+app.get("/home", (req, res)=> {
+    res.send("home")
+})
