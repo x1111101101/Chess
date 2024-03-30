c26514a79363dfcee3d3a6b7e6a2655a703f8b38
x1111101101 <x1111101101@gmail.com>
2024-03-30 15:26:12 +0900

    Signed-off-by: x1111101101 <x1111101101@gmail.com>

--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ const passport = require("passport")
 const GoogleStrategy = require('passport-google-oauth20').Strategy 
 const fs = require('fs');
 const crypto = require('crypto');
+const https = require('https')
 
 const app = express()
-app.use(session({
-    secret: crypto.randomBytes(32).toString('hex'),
-    resave: false,
-    saveUninitialized: true
-}));
+
+
+
+
 function initPassport() {
     app.use(passport.initialize());
     app.use(passport.session());
@@ -20,22 +20,54 @@ function initPassport() {
     passport.use(new GoogleStrategy({
         clientID: oauth.id,
         clientSecret: oauth.key,
-        callbackURL: 'http://playchess.kro.kr:8080/auth/google/callback'
+        callbackURL: 'https://playchess.kro.kr:8080/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
         // 사용자 정보가 profile에 들어 있습니다.
         return done(null, profile);
     }));
+
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+    
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
 }
-initPassport()
+
+
+function init() {
+    app.use(session({
+        secret: crypto.randomBytes(32).toString('hex'),
+        resave: false,
+        saveUninitialized: true
+    }));
+    initPassport()
+    const options = {
+        key: fs.readFileSync('./keys/private.pem'),
+        cert: fs.readFileSync('./keys/public.pem')
+    };
+    return https.createServer(options, app);
+}
+
+const server = init()
 
 app.get('/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] })
 );
-
-app.listen(8080, ()=>{
-    console.log("listening on 8080")
-})
+app.get("/auth/google/callback", passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/login'
+}))
 
 app.get("/home", (req, res)=> {
-    res.send("home")
+    if (req.isAuthenticated()) {
+        res.send(`Hello, ${req.user.displayName}!`);
+    } else {
+        res.send('Hello, guest!');
+    }
+})
+
+server.listen(8080, ()=>{
+    console.log("listening")
 })
